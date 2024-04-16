01aa5d75ab2ca0a7de2e841db2e370aec4eb7238
Dheeraj Jha <106474979+Dheerajjha451@users.noreply.github.com>
2024-04-16 05:46:11 +0530

    Update Upload.jsx

--- src/assets/Components/Upload.jsx
+++ src/assets/Components/Upload.jsx
@@ -27,7 +27,7 @@ function UploadImage() {
     try {
       const formData = new FormData();
       formData.append("file", file);
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://mypbackend.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
